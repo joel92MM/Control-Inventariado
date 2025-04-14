374cf8953c4a26e505e4f59f8c620fea51b67baf
Jesús Joel Meneses Meneses <73592097+joel92MM@users.noreply.github.com>
2025-04-14 15:12:02 +0200

    Se sube la aplicacion en springboot de el curso, control de inventariado

---
+++ Control_Inventario/inventariado/src/main/java/com/aplicacion/inventariado/inventariado/ControlInventariadoApplication.java
@@ -0,0 +1,13 @@
+package com.aplicacion.inventariado.inventariado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlInventariadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlInventariadoApplication.class, args);
+	}
+
+}
